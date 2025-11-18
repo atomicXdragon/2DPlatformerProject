b1747d9f90e689cfe9faa5b2da647b2280440a9a
charlie <charliesims15@outlook.com>
2025-11-18 23:43:13 +0000

    Update LevelHandler.cs

--- Assets/Scripts/LevelHandler.cs
+++ Assets/Scripts/LevelHandler.cs
@@ -8,14 +8,12 @@ public class LevelHandler : MonoBehaviour
     public TextMeshProUGUI levelCounter;
     public static int currentLevel = 0;
     public AudioManager audioManager;
-    public GameManager gameManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = Camera.main.GetComponent<CameraController>();
         audioManager = FindFirstObjectByType<AudioManager>();
-        gameManager = FindFirstObjectByType<GameManager>();
-
         levelCounter.text = currentLevel.ToString();
     }
     // Update is called once per frame
@@ -28,10 +26,7 @@ public class LevelHandler : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             {
-                if (gameManager.life >= 1)
-                {
-                    audioManager.PlaySFX(audioManager.checkPointSound, 0.5f);
-                }
+                audioManager.PlaySFX(audioManager.checkPointSound, 0.5f);
                 collision.transform.position = respawnPoint.position;
                 cam.MoveToNewRoom(nextLevel);
                 currentLevel++;
