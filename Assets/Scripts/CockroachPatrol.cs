58dc887219ee681dc4d9732073a2fb9fee89cf82
charlie <charliesims15@outlook.com>
2025-12-01 16:12:21 +0000

    cockroach anims

--- Assets/Scripts/CockroachPatrol.cs
+++ Assets/Scripts/CockroachPatrol.cs
@@ -18,6 +18,8 @@ public class CockroachPatrol : MonoBehaviour
     private GameManager gameManager;
     private AudioManager audioManager;
     private ScoreManager scoreManager;
+    public Sprite cockroachSquash;
+    public SpriteRenderer spriteRenderer;
     Animator animator;
 
 
@@ -116,6 +118,9 @@ public class CockroachPatrol : MonoBehaviour
         playerRb.linearVelocity = new Vector2(0, bounce);
         isAlive = false;
         rb.linearVelocity = Vector2.zero;
+        spriteRenderer.sprite = cockroachSquash;
+        animator.enabled = false;
+
         yield return new WaitForSeconds(0.5f);
         body.enabled = false;
         Destroy(gameObject);
